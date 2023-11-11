f9746f0b373670a9baf0e171a9e1ae0f8f46c56c
zh2250635 <116276983+zh2250635@users.noreply.github.com>
2023-11-11 23:20:31 +0800

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -5,13 +5,13 @@ myResourceName="azureopenai-test-mine-uksouth"
 myResourceGroupName="openai"
 
 # 获取所有部署的名称列表
-deployments=$(az cognitiveservices account deployment list --name $myResourceName --resource-group $myResourceGroupName | jq -r '.[].name')
+deployments=$(az cognitiveservices account deployment list -g $myResourceGroupName -n $myResourceName | jq -r '.[].name')
 
 # 遍历部署名称并删除每个部署
 for deployment in $deployments
 do
     echo "Deleting deployment $deployment..."
-    az cognitiveservices account deployment delete --name $myResourceName --resource-group $myResourceGroupName --deployment-name $deployment
+    az cognitiveservices account deployment delete -g $myResourceGroupName -n $myResourceName --deployment-name $deployment
     echo "Deleted deployment $deployment"
 done
 
